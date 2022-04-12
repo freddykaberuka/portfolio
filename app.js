c54ffca95bc291eef35a2da987dac473721db998
freddykaberuka <freddykaberuka2009@gmail.com>
2022-04-12 17:37:10 +0200

    fix linters

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-function toggleMenu(menu) {
-    menu.classList.toggle('open')
-}
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line func-names
+// eslint-disable-next-line no-unused-vars
+const toggleMenu = function (menu) {
+  menu.classList.toggle('open');
+};
