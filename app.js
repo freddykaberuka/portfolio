995766723374b0873cee09b8a525abf7fead9e91
YussufOB <hamzyuss1@gmail.com>
2022-04-15 09:12:31 +0000

    update email validator feature script

--- app.js
+++ app.js
@@ -202,7 +202,7 @@ form.addEventListener('submit', (e) => {
   const email = form.email.value;
   if (email.toLowerCase() !== email) {
     e.preventDefault();
-    emailError[0].innerHTML = 'Please Entrer a valid email in lowercase';
+    emailError[0].innerHTML = 'Please provide a valid email in lowercase';
   } else {
     form.action = 'https://formspree.io/f/mwkynqkg';
   }
