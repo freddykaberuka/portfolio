f308a54f9c55d8fbf58a04874fd7be2ca7a1ae9d
freddykaberuka <freddykaberuka2009@gmail.com>
2022-04-15 17:31:30 +0200

    add store and read data in localstorage

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 // form Validation
 const form = document.getElementsByTagName('form')[0];
 const emailError = form.getElementsByTagName('span');
@@ -11,3 +13,18 @@ form.addEventListener('submit', (e) => {
     form.action = 'https://formspree.io/f/mwkynqkg';
   }
 });
+
+// local storage
+
+const store = (data) => {
+  window.localStorage.setItem('formData', JSON.stringify(data));
+  return data;
+};
+const readData = () => {
+  const storedData = window.localStorage.getItem('formData');
+  if (!storedData) return;
+  const data = JSON.parse(storedData);
+  document.getElementById('Names').value = data.Names;
+  document.getElementById('email').value = data.email;
+  document.getElementById('comments').value = data.comments;
+};
